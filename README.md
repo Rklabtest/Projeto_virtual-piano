9e959fd853b075dba6f5b496eaec3631b4a85340
Rklabtest <134552042+Rklabtest@users.noreply.github.com>
2023-11-10 12:59:36 -0300

    Add files via upload
    Adicionando arquivos ao repositório

---
+++ README.md
@@ -0,0 +1,50 @@
+# Projeto Piano Virtual :musical_keyboard:
+
+Este é um projeto de um piano virtual baseado no repositório do Desafio DIO Potência Tech, mas com introdução de novas features.
+
+Diferente do projeto original no qual são incorporados arquivos de som, no presente projeto o som das teclas são gerados por meio do framework de áudio _**Tone.js**_, cuja biblioteca permite trabalhar com uma vasta gama de efeitos e filtros pré-definidos.
+
+## Características do projeto
+* Interruptor Standby-on;
+* Controle de volume;
+* Seletor com 3 opções de som;
+* Guia com as escalas das notas;
+* Controle de visibilidade de marcação de teclas.
+* 24 teclas ativáveis por clique ou teclado. 
+
+Mapa das notas musicais para uso do teclado físico:
+
+
+|Nota |Tecla|   |Nota  |Tecla|
+| :-: | :-: |:-:| :-:  | :-: |
+|  C  |  A  |   |  C+  |  K  |
+|  C# |  Q  |   |  C#+ |  Y  |
+|  D  |  S  |   |  D+  |  L  |
+|  D# |  W  |   |  D#+ |  U  |
+|  E  |  D  |   |  E+  |  Ç  |
+|  F  |  F  |   |  F+  |  M  |
+|  F# |  E  |   |  F#+ |  I  |
+|  G  |  G  |   |  G+  |  ,  |
+|  G# |  R  |   |  G#+ |  O  |
+|  A  |  H  |   |  A+  |  .  |
+|  A# |  T  |   |  A#+ |  P  |
+|  B  |  J  |   |  B+  |  ;  |
+
+
+## Configurações gerais:
+
+O interruptor _Standby--on`_ foi inserido para habilitar a inicialização do contexto de áudio. Isto se fez necessário uma vez que os navegadores exigem que haja uma prévia interação do usuário para a execução de áudios. Além disto, a inserção deste permite liberar recursos, caso a aplicação esteja aberta mas não sendo utilizada.
+
+O array `allowedKeys` armazenado no objeto `piano` contém os nomes das teclas físicas utilizadas para acionamento das teclas do piano. A referência de cada uma é feita por meio dos atributos `data-keys` dos elementos `li` do index.html, e devem receber como valores os códigos das teclas físicas usadas. Deve-se notar que os elementos `<span>` que se aninham em cada um dos elementos `<li>` devem conter o nome da tecla a qual o `data-keys` se refere, estes serão exibidos quando o interruptor `Teclas` estiver acionado.
+
+Os atributos `data-note` referem-se às notas musicais que devem ser executadas e estão dispostas em ordem de acordo com a posição de teclas de um piano real, portanto, não devem ser alterados.
+
+A seleção do tipo de som é feita pela chave _Seletor_. A mudança de valor do `input` atrelado a ele aciona o callback responsável pelo acesso ao `chooseTypeOfSound(option)`, cujas propriedades nele presentes referem-se à criação de um sintetizador específico. 
+
+Ao criar um novo sintetizador, ele é armazenado na propriedade `synth` do objeto `piano`, executado pela chamada da função `activeNote(key)` e é "descartado" sempre que alteramos o seletor, para liberação dos recursos.
+
+Neste projeto, foram incluídos os sintetizadores `Synth`, `MonoSynth` e `FMSynth`, seguindo as recomendações da documentação do [Tone.js](https://tonejs.github.io/) [^1].
+
+O controle de volume é feito pelo `input` da chave _Volume_, que aciona a função `adjustVolume(event)` toda vez que o valor de entrada é alterado. O valor máximo fixado neste projeto é o valor "normal" da saída de áudio e a redução do som se dá pela diminuição de ganho (em dB) aplicado a `synth`. A redução máxima é estabelecida por meio da propriedade `volumeRange` do objeto `piano`.
+
+[^1]: O framework Tone.js inclui o ScriptProcessorMode (interface obsoleta), porém apenas para verificação de recursos, não impactando no processamento de áudio, desta forma, as mensagens de aviso no console referentes a esta interface podem ser ignoradas.  
---
+++ scripts/engine.js
@@ -0,0 +1,149 @@
+const piano = {
+  views: {
+    powerButton: document.querySelector('.power'),
+    volumeControl: document.querySelector('.volume-slider input'),
+    soundSelector: document.querySelector('.sound-selector input'),
+    keysDisplayControl: document.querySelector('.keys-check'),
+    keyboard: document.querySelector('.piano-keys'),
+    keys: document.querySelectorAll('.key')
+  },
+  values: {
+    synth: null,
+    audioContext: null,
+    volumeRange: 40,
+    isKeyPressed: false,
+    allowedKeys: ['a', 's', 'd', 'f', 'g', 'ç', 'l', 'k', 'j', 'h', 'q', 'w', 'e', 'r', 't', 'p', 'o', 'i', 'u', 'y', ';', '.', ',', 'm']
+  },
+  actions: {
+    interval: null
+  }
+}
+
+const initializeAudio = async () => {
+  if (!piano.values.audioContext) {
+    piano.values.audioContext = new AudioContext()
+    Tone.setContext(piano.values.audioContext)
+  }
+
+  try {
+    if (piano.values.audioContext.state === 'suspended') {
+      await Tone.start()
+    }
+    return true
+
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+const chooseTypeOfSound = () => ({
+  '1': new Tone.Synth().toDestination(),
+  '2': new Tone.MonoSynth({
+    oscillator: {type: 'square'},
+    envelope: {attack: 0.1}  
+  }).toDestination(),
+  '3': new Tone.FMSynth().toDestination()
+})
+
+const setSound = option => {
+  piano.values.synth = chooseTypeOfSound()[option]
+  piano.values.synth.volume = -(piano.values.volumeRange/2)
+}
+
+const switchSound = async event => {
+  piano.values.synth.dispose()
+  if(await initializeAudio()) {
+    setSound(event.target.value)
+  }
+}
+
+const activeNote = key => {
+  if (piano.values.isKeyPressed) {
+    piano.values.synth.triggerAttack(key.dataset.note)
+    key.classList.add('active')
+  }
+}
+
+const releaseNote = () => piano.values.synth.triggerRelease()
+
+const stopNote = key => {
+  piano.values.isKeyPressed = false
+  clearInterval(piano.actions.interval)
+  setTimeout(() => key.classList.remove('active'), 150)
+  releaseNote()
+}
+
+const handleClick = event => {
+  if (event.target.dataset.note && piano.values.synth) {
+    piano.values.isKeyPressed = true
+    piano.actions.interval = setInterval(activeNote(event.target), 100)
+  }
+}
+
+const stopHandleClick = event => {
+  if (event.target.dataset.note && piano.values.synth) {
+    stopNote(event.target)
+  }
+}
+
+const handleKeys = event => {
+  if(piano.values.allowedKeys.includes(event.key) && !piano.values.isKeyPressed) {
+    const keyPressed = document.querySelector(`[data-key="${event.keyCode}"]`) 
+    piano.values.isKeyPressed = true
+    piano.actions.interval = setInterval(activeNote(keyPressed), 100)
+  }
+}
+
+const stopHandleKeys = event => {
+  if(piano.values.allowedKeys.includes(event.key)) {
+    const keyPressed = document.querySelector(`[data-key="${event.keyCode}"]`)
+    stopNote(keyPressed)
+  }
+}
+
+const adjustVolume = event => {
+  const volume = (-piano.values.volumeRange + Number(event.target.value)*piano.values.volumeRange)
+  if(volume === -piano.values.volumeRange) {
+    piano.values.synth.volume.value = -Infinity
+  } else {
+    piano.values.synth.volume.value = volume
+  }
+}
+
+const displayKeys = () => {
+  piano.views.keys.forEach(key => key.classList.toggle('hide'))
+}
+
+piano.views.powerButton.addEventListener('click', async () => {
+  if (piano.views.powerButton.hasAttribute('clicked')) {
+    if (piano.values.synth) {
+      piano.values.synth.dispose() 
+      piano.views.soundSelector.removeEventListener('input', switchSound)
+      piano.views.keyboard.removeEventListener('mousedown', handleClick)
+      piano.views.keyboard.removeEventListener('mouseup', stopHandleClick)
+      document.removeEventListener('keydown', handleKeys)
+      document.removeEventListener('keyup', stopHandleKeys)
+      piano.views.volumeControl.removeEventListener('input', adjustVolume)
+    }
+
+    piano.views.powerButton.removeAttribute('clicked')
+    
+  } else {
+
+    piano.views.powerButton.setAttribute('clicked', '')
+    
+    if (await initializeAudio()) {
+      setSound(piano.views.soundSelector.value)
+      piano.views.soundSelector.addEventListener('input', switchSound)
+      piano.views.keyboard.addEventListener('mousedown', handleClick)
+      piano.views.keyboard.addEventListener('mouseup', stopHandleClick)
+      document.addEventListener('keydown', handleKeys)
+      document.addEventListener('keyup', stopHandleKeys)
+      piano.views.volumeControl.addEventListener('input', adjustVolume)
+    } 
+  }
+})
+
+piano.views.keysDisplayControl.addEventListener('click', displayKeys)
+
